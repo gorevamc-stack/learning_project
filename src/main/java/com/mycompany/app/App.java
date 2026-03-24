3e444bf4122775faf85ab9a1bf8ac4dfeb324d52
Maria <gorevamc@gmail.com>
2026-03-24 12:26:55 +0300

    заменено на HttpServer, добавлена проверка запросов

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,47 +1,44 @@
 package com.mycompany.app;
 
-import java.io.*;
-import java.net.ServerSocket;
-import java.net.Socket;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpServer;
 
+import java.io.*;
+import java.net.InetSocketAddress;
 
 public class App {
-    public static void main(String[] args)  {
+    public static void main(String[] args) throws IOException {
 
         int port = 8081;
 
-        try (ServerSocket serverSocket = new ServerSocket(port)) {
+        HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
 
-            System.out.println("Сервер запущен на порту " + port);
+        server.createContext("/ping", (HttpExchange exchange) -> {
 
-            while (true) {
-                Socket clientSocket = serverSocket.accept();
-                try  (BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-                      BufferedWriter out = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()))
-                ) {
-                    String requestLine = in.readLine();
-                    System.out.println("Получен запрос: " + requestLine);
-                    if (requestLine != null && requestLine.startsWith("GET /ping")) {
-                        String responseLine = "pong";
-                        responseServer(out,200,responseLine);
-                        break;
-                    } else {
-                        responseServer(out,404,"Ошибка запроса");
-                    } clientSocket.close();
-                } catch (IOException e) {
-                    System.out.println("Ошибка" + e);
-                }
+            String method = exchange.getRequestMethod();
+
+            if ("GET".equals(method)) {
+                sendResponse(exchange, 200, "pong");
+            } else {
+                sendResponse(exchange, 405, "Неверный метод");
             }
-        } catch (IOException e) {
-            System.out.println("Cервер не запущен" + e);
-        }
+        });
+
+        server.createContext("/", (HttpExchange exchange) ->
+                sendResponse(exchange, 404, "Неизвестный запрос"));
+
+        server.start();
+        System.out.println("Сервер запущен на порту " + port);
     }
 
-    private static void responseServer (BufferedWriter out, int statusCode, String body) throws IOException {
-        out.write("HTTP/1.1 " + statusCode + " OK\r\n" +
-                "Content-Type: text/plain\r\n" +
-                "Content-Length: " + body.getBytes().length + "\r\n" +
-                "\r\n" + body);
-        out.flush();
+    private static void sendResponse(HttpExchange exchange, int status,
+                                     String body) throws IOException {
+        exchange.sendResponseHeaders(status, body.getBytes().length);
+        OutputStream os = exchange.getResponseBody();
+        os.write(body.getBytes());
+        os.close();
+
     }
 }
+
+
